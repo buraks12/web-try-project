20cf8df07adf368b31874c7916b9169eed51734c
buraks12 <buraksakizci0@gmail.com>
2024-07-26 13:38:41 +0300

    base.html problem
    completed

--- template_app/views.py
+++ template_app/views.py
@@ -5,4 +5,12 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return render(request, "first.html")
+    return render(request, "template_app/first.html")
+def weather_view(request):
+    weather_dictionary= {"istanbul" : "30", "amsterdam" : "20",
+                         "paris" : [5,14,20,21], 
+                         "rome" : {"morning" : 20, "evening" : 15},
+                         "user_premium" : True,
+                         "test" : "test Test Test test"
+                         }
+    return render(request, "template_app/weather.html", context=weather_dictionary)
